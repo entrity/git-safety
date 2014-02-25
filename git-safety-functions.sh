e5bd624011118185d2debc8f97952db037cc2e13
Markham <quinesquines@gmail.com>
2014-02-25 13:27:16 -0800

    Fix git config ext.gitsafety check

--- git-safety-functions.sh
+++ git-safety-functions.sh
@@ -2,7 +2,7 @@
 
 # Determine whether this is a git repo that is configured for git-safety extensions
 use_safety_extension=false
-[[ $(git config ext.gitsafety) ]] && use_safety_extension=true
+[[ $(git config ext.gitsafety) == "true" ]] && use_safety_extension=true
 
 
 # Failure message and exit
